b77dac34d1135bf00b725eaff12e84d77f8276e1
nico <nico@Debian10.baronnerie.local>
2020-11-15 17:46:48 +0100

    added settings dialog and cookie service

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CodemirrorModule } from '@ctrl/ngx-codemirror';
+import {CodemirrorComponent, CodemirrorModule} from '@ctrl/ngx-codemirror';
 import {FormsModule} from '@angular/forms';
-import { EditorComponent } from './editor/editor.component';
+import {DialogSettingsComponent, EditorComponent} from './editor/editor.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
+import { CookieService } from 'ngx-cookie-service';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,12 +19,16 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EditorComponent
+    EditorComponent,
+    DialogSettingsComponent,
   ],
   imports: [
     BrowserModule,
@@ -32,10 +41,15 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     MatCardModule,
     MatGridListModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
     HttpClientModule,
+    MatInputModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/editor/editor.component.ts
+++ frontend/src/app/editor/editor.component.ts
@@ -1,34 +1,84 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Component, Inject, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material/dialog';
+import {CookieService} from 'ngx-cookie-service';
+import {CodemirrorComponent, CodemirrorModule} from '@ctrl/ngx-codemirror';
+
+interface Settings {
+  lineNumbers: boolean;
+  theme: string;
+  mode: string;
+  indentUnit: number;
+  tabSize: number;
+  readOnly: boolean;
+  gccFlags: string;
+}
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html'
 })
 export class EditorComponent implements OnInit {
-
+  settings: Settings;
   content = 'int main() {\n\t\n}';
   output = [];
   warning = [];
   error = [];
   lineNumber: true;
-  option = {
-    lineNumbers: true,
-    theme: 'material',
-    mode: 'clike',
-  };
+  option;
 
-  constructor(private http: HttpClient) {
-    this.lineNumber = true;
+  constructor(private http: HttpClient, private dialog: MatDialog, private cookieServer: CookieService) {
+    this.content = this.cookieServer.get('content');
+    if (this.cookieServer.check('settings')) {
+      console.log('COOCKIE SETTINGS');
+      this.settings = JSON.parse(this.cookieServer.get('settings'));
+      console.log(this.settings.theme);
+    } else {
+      console.log('DEFAULT SETTINGS');
+      this.settings = {
+        lineNumbers: true,
+        theme: 'material',
+        mode: 'clike',
+        indentUnit: 2,
+        tabSize: 4,
+        readOnly: false,
+        gccFlags: '',
+      };
+    }
+    if (this.content?.length === 0) {
+      this.content = 'int main() {\n\t\n}';
+    }
+    this.option = {
+      lineNumbers: this.settings?.lineNumbers,
+      theme: this.settings?.theme,
+      mode: 'clike',
+      indentUnit: this.settings?.indentUnit,
+      tabSize: this.settings?.tabSize,
+      readOnly: false
+    };
+  }
+
+  open(): void {
+    const dialog = this.dialog.open(DialogSettingsComponent, {
+      data: this.option
+    });
+    dialog.afterClosed().subscribe(result => {
+      this.settings = result;
+    });
   }
 
   async submit(): Promise<void> {
     console.log(this.output === undefined);
-    const response: any = await this.http.post('http://localhost:3030/uploadjson', {content: this.content}).toPromise();
+    const response: any = await this.http.post('https://app.malloc.studio:3030/uploadjson',
+      {content: this.content}, {headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+      })}).toPromise();
     console.log(response);
     this.output = response.output?.split('\n');
     this.warning = response.warning?.split('\n');
     this.error = response.error?.split('\n');
+    this.cookieServer.set('content', `${this.content}`);
+    console.log(JSON.stringify(this.settings));
   }
 
   onKeyDown(event: any): void {
@@ -37,8 +87,28 @@ export class EditorComponent implements OnInit {
       console.log('submit');
     }
   }
-
   ngOnInit(): void {
   }
+}
 
+@Component({
+  selector: 'app-dialog-settings',
+  templateUrl: 'dialog-settings.html'
+})
+export class DialogSettingsComponent{
+  theme = ['material', 'idea', 'neo', 'base16-dark', '3024-day', '3024-night', 'yeti', 'zenburn', 'yonce', 'xq-light', 'xq-dark'];
+  constructor(public dialogRef: MatDialogRef<DialogSettingsComponent>, @Inject(MAT_DIALOG_DATA) public settings: any, private cookie: CookieService) {
+  }
+  changeTheme(value: string): void {
+    console.log(value);
+    this.settings.theme = value;
+  }
+
+  save(): void {
+    this.cookie.set('settings', JSON.stringify(this.settings));
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
